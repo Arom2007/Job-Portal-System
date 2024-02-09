b4976c7074a3679213dcef1d01a11b33bcb3cf89
Arom2007 <arompradhan@gmail.com>
2024-02-09 17:52:34 +0545

    adding dropdown, adding login and register and adding card titles for the jobs and their description

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic import TemplateView, ListView
+from .models import Category, Job
+class HomeView(ListView):
+    template_name = 'core/home.html'
+    queryset = Job.objects.all()
+    context_object_name = 'jobs'
