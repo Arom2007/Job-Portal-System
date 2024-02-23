74da9a559f2d8fa29d1eab04ef41805645e4e759
Arom2007 <arompradhan@gmail.com>
2024-02-23 16:45:03 +0545

    job details and my jobs

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomeView, job_application
+from .views import HomeView, job_application, MyJobsView, JobDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('job-application/<int:id>/', job_application, name='job_application')
+    path('job-application/<int:id>/', job_application, name='job_application'),
+    path('my-jobs/', MyJobsView.as_view(), name='my_jobs'),
+    path('job-detail/<int:pk>/', JobDetailView.as_view(), name='job_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Category, Job, JobApplication
 from django.shortcuts import redirect
 from django.contrib import messages
@@ -25,3 +25,16 @@ def job_application(request, id):
         JobApplication.objects.create(user=request.user, job_id=id, status="APPLIED")
         messages.success(request, "Successfully applied for the job.")
     return redirect('home')
+
+
+class MyJobsView(ListView):
+    template_name = 'core/my_jobs.html'
+    context_object_name = 'job_applications'
+    def get_queryset(self):
+        return JobApplication.objects.filter(user=self.request.user)
+
+
+class JobDetailView(DetailView):
+    template_name = 'core/job_detail.html'
+    queryset = Job.objects.all()
+    context_object_name = 'job'
